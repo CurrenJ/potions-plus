2ae3100231a86026d4beec5c3f6de239f40f3fe4
Curren <jeandell.curren@gmail.com>
2024-08-26 23:27:42 +0100

    Move scripts

--- src/main/resources/script/0_generate_ore_templates.bat
+++
@@ -1,8 +0,0 @@
-py .\ore_isolator_sat.py input_ore/copper_ore.png 0 1 0 4 1
-py .\ore_isolator_sat.py input_ore/diamond_ore.png 0 1 1 6 3
-py .\ore_isolator_sat.py input_ore/emerald_ore.png 0 1 1 4 2
-py .\ore_isolator_sat.py input_ore/gold_ore.png 0 1 1 6 2
-py .\ore_isolator_sat.py input_ore/lapis_ore.png 0 1 0 6 1
-py .\ore_isolator_sat.py input_ore/redstone_ore.png 0 1 0 6 4
-py .\ore_isolator_sat.py input_ore/iron_ore.png 0 1 0 6 1
-py .\ore_isolator_sat.py input_ore/coal_ore.png 0 1 1 6 -4
